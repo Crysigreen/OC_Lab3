67db72bbc3d6d2d0ebb7b51c0155511cd2dfc6e1
ilya <ilya.korzhov96@gmail.com>
2024-01-15 00:22:27 +0300

    Добавьте файлы проекта.

---
+++ OC_Lab3/OC_Lab3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <Shlwapi.h>
+
+
+int main()
+{
+	//const wchar_t* registryKeyPath = L"Excel.CSV\\shell\\test";
+	const wchar_t* appName = L"C:\\APP\\Notepad++\\notepad++.exe";
+	const wchar_t* iconPath = L"C:\\APP\\Notepad++\\updater\\updater.ico";
+
+	HKEY hKey;
+	if (RegCreateKey(HKEY_CLASSES_ROOT, L"Excel.CSV\\shell\\test", &hKey) == ERROR_SUCCESS) {
+		RegSetValueEx(hKey, L"icon", 0, REG_SZ, (const BYTE*)iconPath, wcslen(iconPath) * 2);
+		RegCloseKey(hKey);
+
+		if (RegCreateKey(HKEY_CLASSES_ROOT, L"Excel.CSV\\shell\\test\\command", &hKey) == ERROR_SUCCESS) {
+			wchar_t command[255];
+			swprintf_s(command, L"%s \"%%1\"", appName);
+			RegSetValueEx(hKey, NULL, 0, REG_SZ, (const BYTE*)command, wcslen(command) * 2);
+			RegCloseKey(hKey);
+		}
+		std::cout << "OK" << std::endl;
+	}
+	else {
+		std::cout << "FAIL" << std::endl;
+	}
+
+	return 0;
+}
